f71f9326e7cfcfc20986c54d7af5bcbc1a7ed059
Lee Crawford <lee.crawford@gmail.com>
2025-12-03 07:01:54 -0800

    Alternate approach

--- src/main/kotlin/day3/Test.kt
+++ src/main/kotlin/day3/Test.kt
@@ -3,6 +3,20 @@ package day3
 fun main () {
     val b = Bank (listOf (2,3,4,2,3,4,2,3,4,2,3,4,2,7,8))
     println (b.largest2(12))
+    println (maximize (b.voltages, 12))
+    return
+}
+
+/**
+ * A functional approach to the maximizer.
+ */
+
+fun maximize (list: List<Int>, pick: Int, acc: Long = 0L): Long {
+    if (pick == 0) {
+        return acc
+    }
+    val max = list.subList (0, list.size - pick + 1).max ()
+    return maximize (list.subList (list.indexOf (max) + 1, list.size), pick - 1, acc * 10 + max)
 }
 
 // EOF

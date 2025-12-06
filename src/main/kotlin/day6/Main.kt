691e89441019f916d697d5dacc9b05c162c4b332
Lee Crawford <lee.crawford@gmail.com>
2025-12-06 08:44:37 -0800

    day6

--- src/main/kotlin/day6/Main.kt
+++ src/main/kotlin/day6/Main.kt
@@ -1,16 +1,99 @@
 package day6
 
-
 import util.InputUtil
+import java.util.*
+import java.util.regex.Pattern
 
 val DAY = 6;
-val SAMPLE = true;
+val SAMPLE = false;
 
 fun main () {
     val input = InputUtil.getInput(DAY, SAMPLE);
-    println (input)
+    val problems = Problems.parse (input)
 
+    // part1
+
+    val part1 = problems.problems.foldRight (0L) { it, acc ->
+        acc + it.value
+    }
+    println (part1)
+
+    // part2
+
+    val part2 = run {
+        val problems = Problems2.parse (input)
+        problems.problems.foldRight (0L) { it, acc ->
+            acc + it.value
+        }
+    }
+    println (part2)
     return
 }
+
+data class Problems (
+    val problems: List<Problem>
+) {
+    companion object {
+        fun parse (str: String): Problems {
+            val rows = str.split ("\n").map {
+                buildList {
+                    it.trim().split(Pattern.compile("\\s+")).forEach { col ->
+                        add (col)
+                    }
+                }
+            }
+            val factors = rows.subList (0, rows.size - 1).map {
+                ArrayDeque (it.map { it.toLong () })
+            }
+            val operations = ArrayDeque<String> (rows[rows.size - 1])
+
+            val problems = buildList {
+                while (operations.isNotEmpty()) {
+                    val f = buildList<Long> {
+                        factors.forEach {
+                            add (it.pop ())
+                        }
+                    }
+                    val o = operations.pop ()[0]
+                    add (Problem (f, Operation.parse (o)))
+                }
+            }
+
+            return Problems (problems)
+        }
+    }
+}
+
+enum class Operation {
+    ADD, MULTIPLY;
+
+    companion object {
+        fun parse (c: Char) = when (c) {
+            '+' -> ADD
+            '*' -> MULTIPLY
+            else -> throw Exception()
+        }
+    }
+}
+
+data class Problem (
+    val factors: List<Long>,
+    val operation: Operation
+) {
+    val value: Long
+        get() = when (operation) {
+            Operation.ADD -> factors.sum()
+            Operation.MULTIPLY -> factors.product ()
+        }
+}
+
+fun List<Long>.product (): Long {
+    var total = 1L
+    forEach {
+        total *= it
+    }
+    return total
+}
+
 
 // EOF
---
+++ src/main/kotlin/day6/Rotate.kt
@@ -0,0 +1,63 @@
+package day6
+
+import util.InputUtil
+import java.util.regex.Pattern
+
+data class Problems2 (
+    val problems: List<Problem>
+) {
+    companion object {
+        fun parse (input: String): Problems2 {
+            val rotated = input.rotated
+            val groups = group (rotated)
+            val list = buildList<Problem> {
+                groups.forEach { group ->
+                    val op = Operation.parse (group[group.length - 1])
+                    val factors = group.substring (0, group.length - 1)
+                        .trim ()
+                        .split (Pattern.compile ("\\s+"))
+                        .map { it.toLong () }
+                    add (Problem (factors, op))
+                }
+            }
+            return Problems2 (list)
+        }
+
+        private fun group (string: String): List<String> {
+            val rows = string.split ("\n").iterator()
+            return buildList {
+                val list = mutableListOf<String> ()
+                while (rows.hasNext ()) {
+                    val row = rows.next ()
+                    if (row.trim ().isEmpty ()) {
+                        add (list.joinToString (""))
+                        list.clear ()
+                    } else {
+                        list.add (row)
+                    }
+                }
+            }
+        }
+    }
+}
+
+val String.rotated: String
+    get () {
+        val parsed = split ("\n")
+        val rows = parsed.size
+        val cols = parsed.map { it.length }.max ()
+        return buildString {
+            for (col in cols - 1 downTo 0) {
+                for (row in 0 until rows) {
+                    try {
+                        append(parsed[row][col])
+                    } catch (e: Exception) {
+                        append (' ')
+                    }
+                }
+                append ("\n")
+            }
+        }
+    }
+
+// EOF

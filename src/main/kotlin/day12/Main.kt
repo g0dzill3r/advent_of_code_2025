8b24aae116f99769666e50c6c90a5a179071394f
Lee Crawford <lee.crawford@gmail.com>
2025-12-12 07:08:27 -0800

    cleanup

--- src/main/kotlin/day12/Main.kt
+++ src/main/kotlin/day12/Main.kt
@@ -9,18 +9,14 @@ fun main () {
     val input = InputUtil.getInput(DAY, SAMPLE)
     val puzzle = Puzzle.parse (input)
 
-    println(puzzle.pieces)
-
     // part1
 
     var part1 = 0
     puzzle.regions.forEach { region ->
         var required = 0
-        println (region)
         region.pieces.forEachIndexed { i, piece ->
             required += piece * puzzle.pieces[i].stars
         }
-        println (region.toString () + " - " +required + " " + region.area)
         if (region.area >= required) {
             part1++
         }
